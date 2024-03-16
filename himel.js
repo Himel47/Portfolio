58ce491b5c3fc7bd7f0107a71b2171807d743337
Md. Shahriar Kabir Himel <death44635@gmail.com>
2024-03-16 13:04:30 +0600

    home done

---
+++ himel.js
@@ -0,0 +1,9 @@
+
+
+var intro = new Typed(".text",{
+    strings:["Web Developer", "Gamer", "Backend Developer", "Youtuber"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    backDelay: 750,
+    loop: true
+});
