b58453d7b9226ab41dc7e9d4f2dca426697eb834
Md. Shahriar Kabir Himel <death44635@gmail.com>
2024-07-26 00:01:57 +0600

    portfolio done

--- Previous/himel.js
+++
@@ -1,9 +0,0 @@
-
-
-var intro = new Typed(".text",{
-    strings:["Web Developer", "Gamer", "Backend Developer", "Youtuber"],
-    typeSpeed: 75,
-    backSpeed: 75,
-    backDelay: 750,
-    loop: true
-});
